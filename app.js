1a1667bbcedc74e27132a11703a3d411c1007767
olatzaranzabe <olatzaranzabe@gmail.com>
2020-06-14 23:40:37 +0200

    change mongoose connection string

--- app.js
+++ app.js
@@ -123,7 +123,7 @@ app.use(function(req, res, next) {
 });
 
 mongoose
-  .connect(process.env.DB_ROUTE, {
+  .connect(process.env.DB_ROUTE.toString(), {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
