cb409edd76649f688ef75dce75a182f1123134cb
olatzaranzabe <olatzaranzabe@gmail.com>
2020-06-18 19:42:43 +0200

    x

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const passport = require('passport');
 // const session = require('express-session');
