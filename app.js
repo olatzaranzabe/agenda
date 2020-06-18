b9014a3ddba0ccf85f791169643f3033ea07f429
olatzaranzabe <olatzaranzabe@gmail.com>
2020-06-18 19:38:24 +0200

    x

--- app.js
+++ app.js
@@ -108,7 +108,7 @@ passport.use(
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
